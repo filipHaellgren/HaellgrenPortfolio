83cba262e0d87f5a5f028a4dcdb300043d190ce7
filipHaellgren <103415366+filipHaellgren@users.noreply.github.com>
2025-05-30 19:10:58 +0200

    Final deploy from /out to root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const calistoga = Calistoga({
 
 export const metadata: Metadata = {
   title: "My Portfolio", 
-  description: "Created with the help of Frontend Tribe",
+  description: "Filip Haellgren's Portfolio",
   viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
