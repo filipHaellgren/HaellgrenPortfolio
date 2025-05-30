a84445d7983fc6746032ac9bb75ab4764dc07b48
filipHaellgren <103415366+filipHaellgren@users.noreply.github.com>
2025-05-30 14:17:14 +0200

    done?

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -20,7 +20,7 @@ export const Footer = () => {
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-amber-700/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
-          <div className="text-[#5C4033]">&copy; 2025 All rights reserved </div>
+          <div className="text-[#5C4033]">&copy; 2025 </div>
           <div>
             <nav className="flex flex-col md:flex-row items-center gap-8"> 
               {footerLinks.map(link => (
